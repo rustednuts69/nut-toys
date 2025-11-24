15c47145ee863d9d35d2cd8c1a96c96ced75f04a
b <jacoluccia46@gmail.com>
2025-11-23 19:29:52 -0600

    removed the unnecessary -r flag in the cp command

--- wallpaper-sync.sh
+++ wallpaper-sync.sh
@@ -2,5 +2,5 @@
 
 # this copies your wallpapers folder to your omarchy theme folder so wallpapers can be added easily
 
-cp -r ~/Pictures/wallpapers/* ~/.config/omarchy/current/theme/backgrounds/
+cp ~/Pictures/wallpapers/* ~/.config/omarchy/current/theme/backgrounds/
 
